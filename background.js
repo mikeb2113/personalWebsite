1ec9cdadb9da499ba592e6c8d8fb7836cb43c665
mikeb2113 <mikebermudez2113@gmail.com>
2023-12-12 22:41:22 -0500

    shrunk down audio files for git to host

--- background.js
+++ background.js
@@ -11,6 +11,8 @@ const rain = 'misc/Rain.mp3';
 let lastPlayedHour = null; // Variable to store the last hour when music was played
 let isMusicPlaying = false;
 let currentAudio = null;
+let fadeOutDuration = 5; // Duration of the fade out in seconds
+let intervalId;
 /*
     private final String musicFilePath = "Animal crossing time music player/City Folk/City Folk %d%s.mp3";
     private final String randomEventFilePath1 = "Animal crossing time music player/misc/Cicada sounds.mp3";
@@ -29,13 +31,13 @@ let currentAudio = null;
     }
 }
 
-    function playMusicAndEvents() {
-        if (!isMusicPlaying) {
-            isMusicPlaying = true;
+function playMusicAndEvents() {
+    if (!isMusicPlaying) {
+        isMusicPlaying = true;
         // Music playback logic
         setInterval(function() {
             let currentHour = getHour(); // Get the current hour
-    
+
             // Check if the current hour is different from the last played hour
             if (lastPlayedHour !== currentHour) {
                 let hour12 = currentHour > 12 ? currentHour - 12 : (currentHour === 0 ? 12 : currentHour);
@@ -43,31 +45,31 @@ let currentAudio = null;
                 let path = musicFilePath.replace('%hour%', hour12).replace('%ampm%', ampm);
                 console.log("path: " + path);
                 playAudio(path); // Play the audio for the current hour
-    
+
                 lastPlayedHour = currentHour; // Update the last played hour
             }
         }, 600);
 
-    // Implementing task2: Random events logic
-    setInterval(function() {
-        let eventCalculator = Math.floor(Math.random() * 100);
-        if (eventCalculator >= 90) {
-            // Logic for one type of event
-            handleEvent(randomEventFilePath1);
-        } else if (eventCalculator >= 85 && eventCalculator < 90) {
-            // Logic for another type of event
-            handleEvent(randomEventFilePath2);
-        }
-    }, 240000);
+        // Implementing task2: Random events logic
+        setInterval(function() {
+            let eventCalculator = Math.floor(Math.random() * 100);
+            if (eventCalculator >= 50) { // Increased chance for this event
+                // Logic for one type of event
+                handleEvent(randomEventFilePath1);
+            } else if (eventCalculator >= 40 && eventCalculator < 50) { // Increased chance for another event
+                // Logic for another type of event
+                handleEvent(randomEventFilePath2);
+            }
+        }, 60000);
 
-    // Implementing task3: Rain event logic
-    setInterval(function() {
-        let random = Math.floor(Math.random() * 100);
-        if (random >= 98) {
-            handleRainEvent(rain);
-        }
-    }, 240000);
-}
+        // Implementing task3: Rain event logic
+        setInterval(function() {
+            let random = Math.floor(Math.random() * 100);
+            if (random >= 50) { // Significantly increased chance for rain event
+                handleRainEvent(rain);
+            }
+        }, 60000);
+    }
 }
 
 function triggerRandomEvent() {
@@ -135,11 +137,45 @@ function playAudio(filePath) {
 
     // Start the new audio track
     currentAudio = new Audio(filePath);
+    currentAudio.loop = false; // Disable auto looping
+
     currentAudio.play().then(() => {
         console.log("Playback started successfully for:", filePath);
     }).catch((error) => {
         console.error("Error during playback for:", filePath, error);
     });
+
+    // Event listener for when the audio ends
+    currentAudio.addEventListener('ended', function() {
+        console.log("Audio track ended. Playing next track.");
+        playNextTrack(); // Function to play the next track
+    });
+}
+
+function playNextTrack() {
+    let currentHour = getHour(); // Assuming getHour() returns the current hour
+    let nextHour = (currentHour + 1) % 24; // Calculate the next hour, wrap around at 24
+    let hour12 = nextHour > 12 ? nextHour - 12 : (nextHour === 0 ? 12 : nextHour);
+    let ampm = nextHour < 12 ? 'am' : 'pm';
+    let path = musicFilePath.replace('%hour%', hour12).replace('%ampm%', ampm);
+    
+    playAudio(path); // Play the audio for the next hour
+}
+
+function fadeOutAudio(audio, duration) {
+    let originalVolume = audio.volume;
+    let step = originalVolume / (duration * 1000 / 10); // Calculate volume decrease step
+    let fadeOutInterval = setInterval(() => {
+        if (audio.volume > step) {
+            audio.volume -= step;
+        } else {
+            // Stop and reset the audio when the volume is almost zero
+            audio.pause();
+            audio.volume = originalVolume;
+            audio.currentTime = 0;
+            clearInterval(fadeOutInterval);
+        }
+    }, 10);
 }
 
 function getHour() {
